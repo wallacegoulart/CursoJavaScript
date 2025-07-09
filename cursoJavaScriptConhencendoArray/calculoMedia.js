4740b75cd9fa9c9a3cd7046e2d1225bebfcd276d
Wallace Goulart <75147050+wallacegoulart@users.noreply.github.com>
2025-07-09 18:16:20 +0000

    Exercicios da aula

---
+++ cursoJavaScriptConhencendoArray/calculoMedia.js
@@ -0,0 +1,10 @@
+const nota01 = 10;
+const nota02 = 6.5;
+const nota03 = 8;
+const nota04 = 7.5;
+
+const notas = [10, 6.5, 8, 7.5];
+
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+
+console.log(media);
